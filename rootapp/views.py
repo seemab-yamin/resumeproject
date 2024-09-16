8dec00b8c43442a39088de60aaaf01192df041e0
Seemab Yamin <80695805+seemab-yamin@users.noreply.github.com>
2024-09-16 14:31:43 +0800

    Updated: Email Roles

--- rootapp/views.py
+++ rootapp/views.py
@@ -4,24 +4,27 @@ from django.core.mail import send_mail, BadHeaderError
 from rootapp.forms import ContactForm
 # Create your views here.
 
+
 def root(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = 'website_inquiry'
+            subject = "website_inquiry"
             body = {
-                'name' : form.cleaned_data['full_name'],
-                'email' : form.cleaned_data['email'],
-                'message' : form.cleaned_data['message']
+                "name": form.cleaned_data["full_name"],
+                "email": form.cleaned_data["email"],
+                "message": form.cleaned_data["message"],
             }
             message = "\n".join(body.values())
 
             try:
-                send_mail(subject, message, 'cbyamin806@gmail.com', ['cbyamin806@gmail.com'])
+                send_mail(
+                    subject, message, "seemab.y1@gmail.com", ["seemab.y1@gmail.com"]
+                )
             except BadHeaderError:
                 return HttpResponse("Invalid Header Found.")
             form = ContactForm()
             # return redirect('/#foot', {'submitted' : True})
-            return render(request, 'rootapp/index.html', {'submitted' : True})
+            return render(request, "rootapp/index.html", {"submitted": True})
     form = ContactForm()
-    return render(request, 'rootapp/index.html', {'form' : form, 'submitted' : False})
+    return render(request, "rootapp/index.html", {"form": form, "submitted": False})
--- static/rootapp/js/main.js
+++ static/rootapp/js/main.js
@@ -7,16 +7,16 @@ $(document).ready(function () {
     //********* typed js
     var element = $(".text-affect");
 
-    $(function() {
+    $(function () {
         element.typed({
-           strings: ["Pythonista.", "Django Developer.", "Designer."],
-           loop: true,
-           typeSpeed: 90
-       });
+            strings: ["Python Developer.", "Django Developer.", "Research Scholar"],
+            loop: true,
+            typeSpeed: 90
+        });
     });
 
     new WOW().init();
-    
+
     $(window).on('scroll', function () {
         var menu_area = $('.nav-area');
         if ($(window).scrollTop() > 200) {
@@ -25,18 +25,18 @@ $(document).ready(function () {
             menu_area.removeClass('sticky_navigation');
         }
     });
-    
+
     $(document).on('click', '.navbar-collapse.in', function (e) {
         if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
             $(this).collapse('hide');
         }
     });
-    
+
     $('body').scrollspy({
         target: '.navbar-collapse',
         offset: 195
     });
-    
+
     $('a.smooth-menu').on("click", function (e) {
         e.preventDefault();
         var anchor = $(this);
@@ -44,74 +44,74 @@ $(document).ready(function () {
             scrollTop: $(anchor.attr('href')).offset().top - 50
         }, 1000);
     });
-    
+
     $('.view').magnificPopup({
-        type:'image',
+        type: 'image',
         gallery: {
             enabled: true
         },
         zoom: {
             enabled: true,
             duration: 300,
-            opener: function(element) {
+            opener: function (element) {
                 return element.find('img');
             }
         }
     });
-    
+
     $('.client-testimonial-carousel').owlCarousel({
         loop: true,
         margin: 10,
         nav: false,
         dots: true,
         autoplay: true,
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:3
+            1000: {
+                items: 3
             }
         }
     })
-    
-    function danex_ripple(){
-      jQuery('.banner-area').ripples({
-        resolution: 500,
-        dropRadius: 20,
-        perturbance: 0.04
-    });
-  }
-  danex_ripple();
 
-  //canvas menu
-  var navexpander = $('#nav-expander');
-  if(navexpander.length){
-    $('#nav-expander').on('click',function(e){
-        e.preventDefault();
-        $('body').toggleClass('nav-expanded');
-    });
-}
-var navclose = $('#nav-close');
-if(navclose.length){
-    $('#nav-close').on('click',function(e){
-        e.preventDefault();
-        $('body').removeClass('nav-expanded');
-    });
-}
+    function danex_ripple() {
+        jQuery('.banner-area').ripples({
+            resolution: 500,
+            dropRadius: 20,
+            perturbance: 0.04
+        });
+    }
+    danex_ripple();
 
-var skillbar = $(".skillbar");
+    //canvas menu
+    var navexpander = $('#nav-expander');
+    if (navexpander.length) {
+        $('#nav-expander').on('click', function (e) {
+            e.preventDefault();
+            $('body').toggleClass('nav-expanded');
+        });
+    }
+    var navclose = $('#nav-close');
+    if (navclose.length) {
+        $('#nav-close').on('click', function (e) {
+            e.preventDefault();
+            $('body').removeClass('nav-expanded');
+        });
+    }
 
-skillbar.waypoint(function () {
-  skillbar.each(function () {
-   $(this).find(".skillbar-child").animate({
-    width: $(this).data("percent")
-}, 1000);
-});
-}, {
-  offset: "80%"
-});
+    var skillbar = $(".skillbar");
+
+    skillbar.waypoint(function () {
+        skillbar.each(function () {
+            $(this).find(".skillbar-child").animate({
+                width: $(this).data("percent")
+            }, 1000);
+        });
+    }, {
+        offset: "80%"
+    });
 });
